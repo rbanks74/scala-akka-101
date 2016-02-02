f5bbedcf80c940e4e292b1ba3df25d115d47122f
rbrutgers <rbanks@viridityenergy.com>
2016-02-02 13:35:35 -0500

    renaming to Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,31 +6,29 @@ import person.Student
 import scala.util.{Failure, Success}
 
 
-object HelloAkkaScala extends App {
+object Main extends App {
 
+  /** Testing creation of Student Instances  */
   val a = new Student("Tim", "Jones", 19)
   println(a)
 
   val b = new Student("Terry", "Simmons", 20)
   println(b)
 
+  /** Testing creation of College Instances  */
   val c = new College("Rutgers University", "New Jersey", 65000, 3.0)
   println(c)
 
+  /** Demonstrating the different ways of creating a list of Student Instances from a csv file  */
   val testFile = tryFile("students.csv" ) match {
     case Success(tf) => enrollStudentsFromCsv(tf)
     case failure@Failure(e) => failure
   }
 
   val test1File = tryFile("students.csv").flatMap(tf => enrollStudentsFromCsv(tf))
-  //println(test1File)
 
   val test2File = for {
     tf <- tryFile("students.csv")
     theList <- enrollStudentsFromCsv(tf)
   } yield theList
-  //println(test2File)
-
-  println(b.getGpa)
-
 }
